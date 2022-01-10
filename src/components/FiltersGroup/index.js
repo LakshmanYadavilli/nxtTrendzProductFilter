8eb5065d445305f99e1b4fc64cb239eae95efcf8
LakshmanYadavilli <lakshmanyadavilli2001@gmail.com>
2022-01-10 14:23:06 +0000

    Assignment

---
+++ src/components/FiltersGroup/index.js
@@ -0,0 +1,40 @@
+import './index.css'
+
+const FiltersGroup = props => {
+  const {ratingsList, categoryOptions, categoryBtn, ratingBtn, input} = props
+  const category = id => {
+    categoryBtn(id)
+  }
+  const rating = id => {
+    ratingBtn(id)
+  }
+  const changeInput = event => {
+    input(event.target.value)
+  }
+  return (
+    <div className="filters-group-container">
+      <h1>Filters Group</h1>
+      <input type="search" onChange={changeInput} />
+      <ul>
+        {categoryOptions.map(n => (
+          <li key={n.categoryId}>
+            <button type="button" onClick={category(n.categoryId)}>
+              {n.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {ratingsList.map(m => (
+          <li key={m.ratingId}>
+            <button type="button" onClick={rating(m.ratingId)}>
+              <img className="img" src={m.imageUrl} alt="rating" />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default FiltersGroup
